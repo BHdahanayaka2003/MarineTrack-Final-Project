296c33bc6bed86e990312374f8d7c9022840f661
AmayaSamarawikcrama <amayasamarawicrama@gmail.com>
2025-04-28 10:47:24 +0530

    Resolved merge conflict in src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-// App.jsx
+
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import FirstPage from './firstpage';
 import LoginPage from './loginpage';
-
+import Dashboard from './Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BoatRegister from './BoatRegister';
 function App() {
   return (
+    
+    //   <div>
+    //   <BoatRegister />
+    // </div>
+    
     <Routes>
+<<<<<<< Updated upstream
       <Route path="/" element={<FirstPage />} />
+=======
+      {/* <Route path="/" element={<HomePage />} />
+>>>>>>> Stashed changes
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/dashboard" element={<Dashboard />} /> */}
+      <Route path="/" element={<Dashboard />} />
+      <Route path='/BoatRegister' element={<BoatRegister />} />
     </Routes>
   );
 }
